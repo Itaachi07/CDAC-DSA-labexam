7f1c491be5c814b838fe664125fb05ca4aa33992
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-05-09 08:36:13 +0530

    Creation of Project and class

---
+++ D4_62758_PratikSatpute/src/Employee.java
@@ -0,0 +1,13 @@
+//Write a program to implement stack of Employee using doubly linear linked list.
+//Every Employee contains information like emp_id, name,age, dept_name and salary.
+
+
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
